b83785e5d992cf6f2404f6ed72d9d7dc294e927a
ryo-techcamp <hurleyx.hp.81rt@outlook.jp>
2020-09-25 00:02:40 +0900

    ユーザー管理機能実装２

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+  validates :nick_name, :password, presence: true
+
+  validates :password, presence: true, length: { minimum: 6 },
+                       format: { with: /\A[a-z0-9]+\z/i, message: 'is must NOT contain any other characters than alphanumerics.' }
+  validates :first_kana, :family_kana, presence: true,
+                                       format: { with: /\A([ァ-ン]|ー)+\z/, message: 'is must NOT contain any other characters than alphanumerics.' }
+  validates :first_name, :family_name, presence: true, format: { with: /\A[一-龥ぁ-ん]/ }
+  has_many :sns_credentials
+      end
